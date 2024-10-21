a3764164f593ef7ece55174f312ecb0cb2914ecd
Stephen Matta <stephenjmatta@gmail.com>
2024-10-21 19:41:38 -0400

    start.spring.io

---
+++ ui-proxy/src/main/java/family/matta/pueblo/ui_proxy/UiProxyApplication.java
@@ -0,0 +1,13 @@
+package family.matta.pueblo.ui_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiProxyApplication.class, args);
+	}
+
+}
---
+++ ui-proxy/src/test/java/family/matta/pueblo/ui_proxy/UiProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package family.matta.pueblo.ui_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
